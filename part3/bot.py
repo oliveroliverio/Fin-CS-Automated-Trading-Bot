59d555805060095da038eba620d97b42d2d2fe4a
Oliver Oliverio <oliveroliverio@gmail.com>
2021-11-15 15:57:42 -0800

    20211115 part3 fixed bug (dot on format)

--- part3/bot.py
+++ part3/bot.py
@@ -1,5 +1,11 @@
 import websocket, json
 
+minutes_processed = {}
+minute_candlesticks = []
+current_tick = None
+# store previous tick as the close of the last candle
+previous_tick = None
+
 def on_open(ws):
   print("opened connection")
   subscribe_message = {
@@ -12,8 +18,16 @@ def on_open(ws):
   ws.send(json.dumps(subscribe_message))
 
 def on_message(ws, message):
-  print("recieved message")
-  print(message)
+  global current_tick, previous_tick
+  # when you see a message
+  previous_tick = current_tick
+  current_tick = json.loads(message)
+
+  # print debugging info
+  print("=== Recieved Tick ===")
+  # print formatted string of the timestamp and price
+  print("{} @ {}".format(current_tick['time'], current_tick['price']))
+
 socket = "wss://ws-feed.pro.coinbase.com"
 ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message)
 ws.run_forever()
